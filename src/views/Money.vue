0a9a5c252966b66da76a241f02e08fc378593caa
sherry <1936839308@qq.com>
2020-04-26 18:17:40 +0800

    money布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    money
+  <div class="money-mav">
+    <div class="content">
+      money
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+.money-mav{
+  border:2px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
