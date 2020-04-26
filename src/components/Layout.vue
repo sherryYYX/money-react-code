fb822194ed63e52a72b31fef407092f0effaba20
sherry <1936839308@qq.com>
2020-04-26 23:08:51 +0800

    nav css

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -15,13 +15,11 @@
 
 <style lang="scss" scoped>
   .nav-wrapper{
-    border:2px solid green;
     display: flex;
     flex-direction: column;
     height: 100vh;
   }
   .content{
-    border: 1px solid blue;
     flex-grow: 1;
     overflow: auto;
   }
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money" >
+  <nav class="nav">
+    <router-link to="/money" class="item">
      <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" > <Icon name="label"/>标签</router-link>
-    <router-link to="/statistics" >  <Icon name="statistics"/>统计</router-link>
-  </div>
+    <router-link to="/labels" class="item">
+      <Icon name="label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -17,7 +23,24 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border: 1px solid red;
+  nav{
+    font-size: 15px;
+    /*border: 1px solid red;*/
+    display: flex;
+    box-shadow: 0 0 3px rgb(0,0,0,0.25);
+    flex-direction: row;
+    > .item{
+      padding: 2px;
+      width: 33.33333%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        height: 34px;
+        width: 34px;
+      }
+    }
   }
+
 </style>
