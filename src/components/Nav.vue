a0cd0c2679ade9b55d4b6a371207ba80752853ab
sherry <1936839308@qq.com>
2020-04-27 09:39:50 +0800

    svg remove fill

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="nav">
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
      <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -41,6 +41,10 @@
         width: 34px;
       }
     }
+    > .item.selected{
+      color: #ff0200;
+    }
+
   }
 
 </style>
